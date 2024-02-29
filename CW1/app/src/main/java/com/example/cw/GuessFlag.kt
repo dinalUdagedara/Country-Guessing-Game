6bb003c34e5ab778023ed645f625cb548169f0bf
Thiyunu <dinal.bandara@gmail.com>
2024-02-29 18:40:08 +0530

    Cloned project to the Repository

---
+++ CW1/app/src/main/java/com/example/cw/GuessFlag.kt
@@ -0,0 +1,36 @@
+package com.example.cw
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cw.ui.theme.CwTheme
+
+class GuessFlag : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CwTheme {
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ){
+
+                    Text(text = "Welcome To Guess Flag Page")
+                }
+            }
+        }
+    }
+}
+
---
+++ CW1/app/src/main/java/com/example/cw/MainActivity.kt
@@ -0,0 +1,232 @@
+package com.example.cw
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.RoundedCorner
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.cw.ui.theme.CwTheme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CwTheme {
+
+                val expanded = remember { mutableStateOf(false) }
+                val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+                Surface(
+
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+
+                ) {
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(24.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+
+
+                            ) {
+                            Surface(
+
+
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(8.dp),
+                                color = MaterialTheme.colors.surface,
+                                elevation = 4.dp,
+                                border = BorderStroke(1.dp, Color.Gray)
+                            ){
+
+                            Row(
+                                modifier = Modifier
+                                    .padding(16.dp)) {
+                                Column(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(25.dp)
+                                ) {
+                                    Text(text = "Guess The Country")
+                                }
+                                Button(onClick = {
+                                    val navigate = Intent(this@MainActivity, GuessCountry::class.java)
+                                    startActivity(navigate)
+                                },
+                                    modifier = Modifier.padding(10.dp),
+                                    elevation = ButtonDefaults.elevation(8.dp),
+                                    shape = RoundedCornerShape(50), // Adjust the corner radius as needed
+                                    colors = ButtonDefaults.buttonColors(
+                                        backgroundColor = MaterialTheme.colors.primary,
+                                        contentColor = MaterialTheme.colors.onPrimary
+                                )
+                                        ){
+                                    Text(text = "Start",
+                                        modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp))
+                                }
+
+
+                            }
+                        }
+
+                            Surface(
+
+
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(8.dp),
+                                color = MaterialTheme.colors.surface,
+                                elevation = 4.dp,
+                                border = BorderStroke(1.dp, Color.Gray)
+                            ){
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(16.dp)) {
+                                    Column(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .padding(25.dp)
+                                    ) {
+                                        Text(text = "Guess-Hints")
+                                    }
+                                    Button(onClick = {
+                                        val navigate = Intent(this@MainActivity, GuessHints::class.java)
+                                        startActivity(navigate)
+                                    },
+                                        modifier = Modifier.padding(10.dp),
+                                        elevation = ButtonDefaults.elevation(8.dp),
+                                        shape = RoundedCornerShape(50), // Adjust the corner radius as needed
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = MaterialTheme.colors.primary,
+                                            contentColor = MaterialTheme.colors.onPrimary
+                                        )
+                                    ){
+                                        Text(text = "Start",
+                                            modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp))
+                                    }
+
+
+                                }
+                            }
+
+
+                            Surface(
+
+
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(8.dp),
+                                color = MaterialTheme.colors.surface,
+                                elevation = 4.dp,
+                                border = BorderStroke(1.dp, Color.Gray)
+                            ){
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(16.dp)) {
+                                    Column(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .padding(25.dp)
+                                    ) {
+                                        Text(text = "Guess the Flag")
+                                    }
+                                    Button(onClick = {
+                                        val navigate = Intent(this@MainActivity, GuessFlag::class.java)
+                                        startActivity(navigate)
+                                    },
+                                        modifier = Modifier.padding(10.dp),
+                                        elevation = ButtonDefaults.elevation(8.dp),
+                                        shape = RoundedCornerShape(50), // Adjust the corner radius as needed
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = MaterialTheme.colors.primary,
+                                            contentColor = MaterialTheme.colors.onPrimary
+                                        )
+                                    ){
+                                        Text(text = "Start",
+                                            modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp))
+                                    }
+
+
+                                }
+                            }
+
+
+
+
+                            Surface(
+
+
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(8.dp),
+                                color = MaterialTheme.colors.surface,
+                                elevation = 4.dp,
+                                border = BorderStroke(1.dp, Color.Gray)
+                            ){
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(16.dp)) {
+                                    Column(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .padding(25.dp)
+                                    ) {
+                                        Text(text = "Advanced Level")
+                                    }
+                                    Button(onClick = {
+                                        val navigate = Intent(this@MainActivity, AdvancedLevel::class.java)
+                                        startActivity(navigate)
+                                    },
+                                        modifier = Modifier.padding(10.dp),
+                                        elevation = ButtonDefaults.elevation(8.dp),
+                                        shape = RoundedCornerShape(50), // Adjust the corner radius as needed
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = MaterialTheme.colors.primary,
+                                            contentColor = MaterialTheme.colors.onPrimary
+                                        )
+                                    ){
+                                        Text(text = "Start",
+                                            modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp))
+                                    }
+
+
+                                }
+                            }
+
+
+
+                        }
+
+                }
+            }
+        }
+    }
+}
+
+
+
